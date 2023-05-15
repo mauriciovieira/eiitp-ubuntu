897289fb2a0b5f982ed0d68b37c5895393888312
Maurício Vieira <mauricio@mauriciovieira.net>
2023-05-15 15:24:07 -0300

    fix: update functions
    It was not running the server because of user permissions

--- pg_util_funcs.zsh
+++ pg_util_funcs.zsh
@@ -1,10 +1,12 @@
 function switch_pg_database() {
-  sudo systemctl stop postgresql
+  sudo systemctl stop postgresql@14-main
   sudo rm /var/lib/postgresql/14/main
   sudo ln -s /var/lib/postgresql/14/main-$1 /var/lib/postgresql/14/main
-  sudo systemctl start postgresql
+  sudo chown -h postgres:postgres /var/lib/postgresql/14/main
+  sudo systemctl start postgresql@14-main
 }
 
 function create_pg_database() {
   sudo cp -r /var/lib/postgresql/14/main-$1 /var/lib/postgresql/14/main-$2
+  sudo chown -R postgres:postgres /var/lib/postgresql/14/main-$2
 }
