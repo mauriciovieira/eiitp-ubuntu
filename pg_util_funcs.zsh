049f28ec744b77907d79071f3a51de7f34f9a409
Maurício Vieira <mauricio@mauriciovieira.net>
2023-05-22 15:05:52 -0300

    feat: add new functions

--- pg_util_funcs.zsh
+++ pg_util_funcs.zsh
@@ -11,6 +11,10 @@ function create_pg_database() {
   sudo chown -R postgres:postgres /var/lib/postgresql/14/main-$2
 }
 
+function remove_pg_database() {
+  sudo rm -rf /var/lib/postgresql/14/main-$1
+}
+
 function current_pg_database() {
   basename $(readlink -f /var/lib/postgresql/14/main)
 }
@@ -19,4 +23,17 @@ function list_pg_databases() {
   for dir in /var/lib/postgresql/14/main-*; do
     echo "$(basename $dir) - $(stat -c %y $dir)"
   done
+}
+
+function reset_pg_database() {
+  echo "> Current pg database"
+  current_pg_database
+  echo "> Switching to master"
+  switch_pg_database master
+  echo "> Removing $1"
+  remove_pg_database $1
+  echo "> Creating database $1"
+  create_pg_database master $1
+  echo "> Switching to $1"
+  switch_pg_database $1
 }
